21c8a1eca24a5bbfcd13afa4cc92cededd7d7f89
Rani <ranip130303@gmail.com>
2024-05-30 23:08:24 +0530

    slight modification done

--- app.py
+++ app.py
@@ -59,5 +59,5 @@ def upload():
 def display_image(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "_main_":
+    app.run(host='0.0.0.0', port=5000,debug=True)
